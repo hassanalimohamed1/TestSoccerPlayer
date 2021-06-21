0324be0c34b6f1d235226e767d08e716ad21d730
Hassan Mohamed <hassanalimohamed1@gmail.com>
2021-06-21 12:10:50 +1000

    First Commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+namespace TestSoccerPlayer
+{
+    class TestSoccerPlayer
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello SoccerPlayer World!");
+            SoccerPlayer myFav = new SoccerPlayer();
+            myFav.Name = "Messi";
+            myFav.JerseyNumber = 10;
+            myFav.Goals = 750;
+            myFav.Assist = 100;
+
+            WriteLine($"My Favorite player is {myFav.Name}, with jersey number { myFav.JerseyNumber}, who has scored { myFav.Goals} and {myFav.Assist } assists.");
+        }
+    }
+
+    class SoccerPlayer
+    {
+        public string Name { get; set; }
+        public int JerseyNumber { get; set; }
+        public int Goals { get; set; }
+        public int Assist { get; set; }
+    }
+ 
+}
